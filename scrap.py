b1d1dc72d3cfa125d6a0e3b2d113cfb7edd15f60
irina-dibceva <github.com/irina-dibceva/pycharm-projects>
2019-09-27 14:20:19 +0300

    home-work for weekend

--- scrap.py
+++ scrap.py
@@ -1,4 +1,5 @@
 import codecs
+import time
 
 import requests
 from bs4 import BeautifulSoup as BS
@@ -9,7 +10,7 @@ headers = {'User-Agent': 'Mozilla/0.5 (Windows NT 5.1; rv:47.0) Gecko/20100101 F
 url = 'http://loveread.ec/index.php?id_genre=14'
 url1 = 'http://www.ukrchess.org.ua/'
 
-domain = 'http://loveread.ec'
+domain = 'http://loveread.ec/index_book.php?id_genre=14&p='
 aa = []
 urls = [url]
 req = session.get(url, headers=headers)
@@ -17,24 +18,56 @@ req1 = session.get(url1, headers=headers)
 
 if req.status_code == 200:
     bsObj = BS(req.content, 'html.parser')
+    pagination = bsObj.find('div', attrs={'class': 'navigation'})
+    pages = pagination.find_all('a')
+    for page in pages:
+        p = page.text
+        #print(p)
+        if p != 'Вперед' :
+            urls.append(domain + p)
+            print(urls)
+
+
+for ur in urls:
+    time.sleep(2)
+    req = session.get(ur, headers=headers)
+    if req.status_code == 200:
+        bsObj = BS(req.content, 'html.parser')
+        table_list = bsObj.find_all('table', attrs={'class': 'table_gl'})
+        div_list = bsObj.find_all('div', attrs={'class': 'td_top_text'})
+        td_list = bsObj.find_all('td', attrs={'class': 'span_str'})
+        for div in table_list:
+            title = div.find('strong')
+            td = div.find('td', attrs={'class': 'span_str'})
+            descr = td.p.text
+            aa.append({'title': title, 'descr': descr})
+
+
+
+
+if req.status_code == 200:
+    bsObj = BS(req.content, 'html.parser')
     div_list = bsObj.find_all('div', attrs={'class': 'td_top_text'})
-    #print(div_list.text)
     for div in div_list:
         title = div.find('strong')
+        #print(title.text)
+    td_list = bsObj.find_all('td', attrs={'class':'span_str'})
+
+    #for td in td_list:
+        #descr = td.p.text
+        #print(descr)
         #href = div.a['href']
-        print(title.text)
+        #print(title.text)
 
-if req1.status_code == 200:
-    bsObj = BS(req1.content, 'html.parser')
-    div_list = bsObj.find_all('div', attrs={'class': 'anons'})
-    #print(div_list.text)
-    for div in div_list:
-        title = div.find('span')
-        #href = div.a['href']
-        print(title.text)
 
-#data = bsObj.prettify()#.encode('utf8')
 
-   # handle = codecs.open('1.html', 'w', 'utf-8')
-    #handle.write(str(div_list))
- #handle.close()
+template = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"></head><body>'
+end = '</body></html>'
+content = '<h2> LoveRead</h2>'
+for job in aa:
+    content += '<a target="_blank">{title}</a><br/><p>{descr}</p><br/>'.format(**job)
+    content += '<hr/><br/><br/>'
+data = template + content + end
+handle = codecs.open('1.html', 'w', 'utf-8')
+handle.write(str(data))
+handle.close()
